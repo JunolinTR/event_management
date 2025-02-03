2cd6da0c83929fb42fa45734cbaac765c854c0c3
Akshay <140790680+Akshay-006@users.noreply.github.com>
2025-02-03 22:27:29 +0530

    Update Events.jsx

--- client/src/pages/Events.jsx
+++ client/src/pages/Events.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
+import axios from "axios"
+import { useState } from 'react'
+import { useEffect } from 'react';
+
 
 function Events() {
+  const [ListOfEvents, setEvents] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/events/").then((response) => {
+      setEvents(response.data);
+    });
+  }, [])
   return (
     <div>
-      
+      {ListOfEvents.map((value,key) => {
+        return <div>
+          <div className='name'>{value.event_name}</div>
+          <div className='location'>{value.location}</div>
+          </div>
+      })}
     </div>
   )
 }
 
-export default Events
+export default Events;
